f91a6d514d125c6f523e5b1ab382f19de105b031
Naeem Shaikh <naeemshaikh90@yahoo.co.in>
2017-01-11 21:48:02 +0530

    Add TestCases

---
+++ BabylonHealthDemoTests/Models/PostSpec.swift
@@ -0,0 +1,35 @@
+//
+//  PostSpec.swift
+//  BabylonHealthDemo
+//
+//  Created by Naeem Shaikh on 11/01/17.
+//  Copyright © 2017 Naeem Shaikh. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import BabylonHealthDemo
+
+class PostSpec: QuickSpec {
+  override func spec() {
+    describe("a post") {
+      
+      var post: BabylonHealthDemo.Post!
+      
+      beforeEach {
+        let testBundle = Bundle(for: type(of: self))
+        let mockLoader = MockLoader(file: "PostSpec", in: testBundle)
+        post = mockLoader?.map(to: Post.self)
+        dump(post)
+      }
+      
+      it("should be able to create a post from json") {
+        expect(post).toNot(beNil())
+      }
+      
+      it("should have a title") {
+        expect(post.title).toNot(beNil())
+      }
+    }
+  }
+}
